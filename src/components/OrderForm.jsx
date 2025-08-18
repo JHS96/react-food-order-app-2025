7a8baa7d8739b86954c2f9cb51c5dc2de635f7cd
JHS96 <jschaper@hotmail.co.za>
2025-08-18 12:41:09 +0200

    Keep entered data on form upon invalid form submission

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,3 +1,5 @@
+import { useActionState } from 'react';
+
 import {
   isEmail,
   isEmpty,
@@ -5,74 +7,124 @@ import {
   isNumber,
 } from '../util/validation.js';
 
-export default function OrderForm({ orderTotal, setModalMode }) {
-  function handleGoBack() {
-    setModalMode('cart');
+// formData is automatically passed to the function similar to "event"
+// prevFormState is not needed here, but must be accepted as 1st arg to prevent error. formData must
+// the 2nd arg when using the useActionState hook
+function orderAction(prevFormState, formData) {
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const street = formData.get('street');
+  const postalCode = formData.get('postal-code');
+  const city = formData.get('city');
+
+  let errors = {};
+
+  if (isEmpty(name) || !hasMinLength(name, 1)) {
+    errors.name = 'Your full name must be at least 1 character.';
   }
 
-  function orderAction(formData) {
-    const enteredName = formData.get('name');
-    const enteredEmail = formData.get('email');
-    const enteredStreet = formData.get('street');
-    const enteredPostalCode = formData.get('postal-code');
-    const enteredCity = formData.get('city');
+  if (!isEmail(email) || isEmpty(email)) {
+    errors.email = 'Please provide a valid email address.';
+  }
 
-    let errors = {};
+  if (!hasMinLength(street, 2)) {
+    errors.street = 'Your street name must be at least 2 characters.';
+  }
 
-    if (isEmpty(enteredName) || !hasMinLength(enteredName, 1)) {
-      errors.name = 'Your full name must be at least 1 character.';
-    }
+  if (!hasMinLength(postalCode, 4) || !isNumber(postalCode)) {
+    errors.postalCode = 'Please enter a valid postal code.';
+  }
 
-    if (!isEmail(enteredEmail) || isEmpty(enteredEmail)) {
-      errors.email = 'Please provide a valid email address.';
-    }
+  if (!hasMinLength(city, 2)) {
+    errors.city = 'Your city name must be at least 2 characters.';
+  }
+  console.log(errors);
+  const errorsLength = Object.keys(errors).length;
 
-    if (!hasMinLength(enteredStreet, 2)) {
-      errors.street = 'Your street name must be at least 2 characters.';
-    }
+  if (errorsLength > 0) {
+    return {
+      errors,
+      enteredValues: {
+        name,
+        email,
+        street,
+        postalCode,
+        city,
+      },
+    };
+  }
 
-    if (!hasMinLength(enteredPostalCode, 4) || !isNumber(enteredPostalCode)) {
-      errors.postalCode = 'Please enter a valid postal code.';
-    }
+  return { errors: null };
 
-    if (!hasMinLength(enteredCity, 2)) {
-      errors.city = 'Your city name must be at least 2 characters.';
-    }
+  // submit form if valid
+  // close form after valid submission
+  // clear state in context after valid submission
+}
 
-    console.log(errors);
+export default function OrderForm({ orderTotal, setModalMode }) {
+  // args for useActionState hook: 1st => actionFn to be executed, then 2nd => initial state for in case the actionFn has not been executed yet
+  // useActionState returns an array with 3 items (3rd to be added later here)
+  // 1st => formState = the state of the form
+  // 2nd => formAction = enhanced form of actionFn (orderAction in this case)
+  // 3rd => isPending = not yet used here
+  const [formState, formAction] = useActionState(orderAction, { errors: null });
 
-    // don't clear form if submit is attempted but form is invalid
-    // submit form if valid
-    // close form after valid submission
-    // clear state in context after valid submission
+  function handleGoBack() {
+    setModalMode('cart');
   }
 
   return (
     <>
       <h2>Checkout</h2>
       <p>Total Amount: ${Math.max(0, orderTotal).toFixed(2)}</p>
-      <form action={orderAction} noValidate>
+      {/* formAction must be passed to action here as react uses it to wrap/enhance the developer defined orderAction */}
+      <form action={formAction} noValidate>
         <div className='inputs'>
           <div className='control'>
             <label htmlFor='name'>Full Name</label>
-            <input type='text' name='name' id='name' />
+            <input
+              type='text'
+              name='name'
+              id='name'
+              defaultValue={formState.enteredValues?.name}
+            />
           </div>
           <div className='control'>
             <label htmlFor='email'>E-Mail Address</label>
-            <input type='email' name='email' id='email' />
+            <input
+              type='email'
+              name='email'
+              id='email'
+              defaultValue={formState.enteredValues?.email}
+            />
           </div>
           <div className='control'>
             <label htmlFor='street'>Street</label>
-            <input type='text' name='street' id='street' />
+            <input
+              type='text'
+              name='street'
+              id='street'
+              defaultValue={formState.enteredValues?.street}
+            />
           </div>
           <div className='control-row'>
             <div className='control'>
               <label htmlFor='postal-code'>Postal Code</label>
-              <input type='number' name='postal-code' id='postal-code' />
+              <input
+                type='number'
+                name='postal-code'
+                id='postal-code'
+                defaultValue={formState.enteredValues?.postalCode}
+              />
             </div>
             <div className='control'>
               <label htmlFor='city'>City</label>
-              <input type='text' name='city' id='city' />
+              <input
+                type='text'
+                name='city'
+                id='city'
+                defaultValue={formState.enteredValues?.city}
+              />
             </div>
           </div>
         </div>
